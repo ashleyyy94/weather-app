5a34228a66b6cbaf5c3d7a1a9603ba7149721c97
Ashley Ong <ashley_2461994@hotmail.com>
2022-10-15 22:22:10 +0800

    remove unused classes

--- src/SearchItem.js
+++ src/SearchItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
